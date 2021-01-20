068953a14b9475990a59135c97c26b6b76e06a85
matthewogtong <ogtongm@gmail.com>
2021-01-19 20:05:30 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushis, setSushis] = useState([])
+  const [wallet, setWallet] = useState(100)
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(sushisArr => {
+        const updatedSushisArr = sushisArr.map(sushi => {
+          return {
+            ...sushi,
+            eaten: false
+          }
+        })
+        setSushis(updatedSushisArr)
+      })
+  }, [])
+
+  console.log(sushis)
+
+  const handleEatenSushi = (eatenSushi) => {
+    if (!eatenSushi.eaten && wallet > eatenSushi.price) {
+      const updatedSushis = sushis.map(sushi => {
+        if (sushi.id === eatenSushi.id) {
+          return {
+            ...sushi, 
+            eaten: true
+          }
+        } else {
+          return sushi
+        }
+      })
+      setSushis(updatedSushis)
+      setWallet(wallet - eatenSushi.price)
+    } else {
+      alert("you cannot purchase this sushi")
+    }
+  }
+
+  const eatenSushis = sushis.filter(sushi => {
+    return sushi.eaten
+  })
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} handleEatenSushi={handleEatenSushi}/>
+      <Table plates={eatenSushis} wallet={wallet}/>
     </div>
   );
 }
